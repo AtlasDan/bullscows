dc4e0abd278d87ca09a7f6eb7a96fc1380c3f2b0
AtlasDan <nikolaevdaniiloleg@gmail.com>
2021-01-04 00:08:56 +0700

    The number.h file declares functions for working with numbers

--- src/sources/number.h
+++ src/sources/number.h
@@ -11,5 +11,8 @@
 using namespace std;
 
 int getRandNum(int min, int max);
+int getUserNum(int minUserInputNum, int maxUserInputNum);
+vector<int> getNumSymbols(int number);
+bool compareNumbers(int hiddenNum, int userNumInput);
 
 #endif // BULLCOW_NUMBER_H
