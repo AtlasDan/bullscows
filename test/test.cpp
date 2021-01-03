3012d0f7a4ed45214fa7177d0e3ce5b0a53288e5
AtlasDan <nikolaevdaniiloleg@gmail.com>
2021-01-04 00:18:49 +0700

    Additional unit tests have been written

--- test/test.cpp
+++ test/test.cpp
@@ -2,6 +2,36 @@
 #define CATCH_CONFIG_MAIN
 #include "../thirdparty/catch/catch.hpp"
 
+TEST_CASE("Game win (true)")
+{
+	int userNum = 1234;
+	int hiddenNum = 1234;
+	bool expected = true;
+    REQUIRE(compareNumbers(hiddenNum, userNum) == expected);
+}
+
+TEST_CASE("Game win (false)")
+{
+	int userNum = 4321;
+	int hiddenNum = 1234;
+	bool expected = false;
+    REQUIRE(compareNumbers(hiddenNum, userNum) == expected);
+}
+
+TEST_CASE("Get symbols vector (true)")
+{
+	vector<int> expected = {1, 2, 3, 4};
+	vector<int> turned = getNumSymbols(1234);
+    REQUIRE(turned == expected);
+}
+
+TEST_CASE("Get symbols vector (false)")
+{
+	vector<int> expected = {1, 2, 3, 4};
+	vector<int> turned = getNumSymbols(4321);
+    REQUIRE(turned != expected);
+}
+
 TEST_CASE("Get random number (more)")
 {
 	int min = 1000;
