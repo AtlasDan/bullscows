6a004d283f94bf3587829d35d13ebeb4c3874f21
AtlasDan <nikolaevdaniiloleg@gmail.com>
2021-01-03 23:17:29 +0700

    The game library has been created and described. h. The library contains the game initialization function.

---
+++ src/sources/game.h
@@ -0,0 +1,15 @@
+#ifndef BULLCOW_GAME_H
+#define BULLCOW_GAME_H
+
+#define MAX_ATTEMPT -1
+#define LENGTH_NUM 4
+#define DEVELOP_MODE false
+
+#include "message.h"
+#include "number.h"
+
+using namespace std;
+
+void game();
+
+#endif // BULLCOW_GAME_H
