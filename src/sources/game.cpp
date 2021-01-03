f508d9744e93b9566b4a51be605604457ee021a3
AtlasDan <nikolaevdaniiloleg@gmail.com>
2021-01-03 23:52:18 +0700

    In the file game.cpp basic game settings have been created

--- src/sources/game.cpp
+++ src/sources/game.cpp
@@ -5,5 +5,17 @@
  */
 void game()
 {
-    
+    srand(time(nullptr));
+
+    int userMenuInput;
+    int userNumInput;
+    int currentAttempt;
+    int hiddenNum;
+
+    /******* Default game options *******/
+    int maxAttempt = MAX_ATTEMPT;
+    int lengthNum = LENGTH_NUM;
+    int minUserInputNum = pow(10, lengthNum - 1);
+    int maxUserInputNum = pow(10, lengthNum) - 1;
+    /***********************************/
 }
