f386df5b270e5120286205538e6d82ff26b8e486
AtlasDan <nikolaevdaniiloleg@gmail.com>
2021-01-04 00:07:35 +0700

    In the file game.cpp created a basic game cycle with basic functionality

--- src/sources/game.cpp
+++ src/sources/game.cpp
@@ -20,4 +20,44 @@ void game()
     /***********************************/
 
     gameHeader();
+
+    do {
+        currentAttempt = 1;
+
+        mainMenu();
+        cin >> userMenuInput;
+
+        if (userMenuInput == 1) {
+            hiddenNum = getRandNum(minUserInputNum, maxUserInputNum);
+
+            if (DEVELOP_MODE) {
+                cout << endl << hiddenNum;
+            }
+
+            do {
+                inputNumber(currentAttempt, maxAttempt);
+                userNumInput = getUserNum(minUserInputNum, maxUserInputNum);
+
+                if (userNumInput == 0) {
+                    break;
+                }
+
+                if (!compareNumbers(hiddenNum, userNumInput)) {
+                    currentAttempt++;
+                    if (maxAttempt + 1 == currentAttempt) {
+                        lostGame();
+                        break;
+                    }
+                } else {
+                    congratulate(hiddenNum);
+                    break;
+                }
+            } while (true);
+        } else if (userMenuInput == 0) {
+            goodbye();
+            break;
+        } else {
+            invalidInput();
+        }
+    } while (userMenuInput != 0);
 }
