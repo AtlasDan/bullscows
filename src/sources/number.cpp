0c75244ee9f1bf2dea6a324fe86c70a725b58fef
AtlasDan <nikolaevdaniiloleg@gmail.com>
2021-01-03 23:33:38 +0700

    In the file number.cpp the function of generating a random number of integer type is implemented

--- src/sources/number.cpp
+++ src/sources/number.cpp
@@ -1,1 +1,14 @@
 #include "number.h"
+
+/**
+ * Генерация случайного числа в заданном диапазоне
+ * @param min Минимальное число
+ * @param max Максимальное число
+ * @return Случайное число
+ */
+int getRandNum(int min, int max)
+{
+    return rand() * (1.0 / static_cast<double>(RAND_MAX + 1.0))
+            * (max - min + 1)
+            + min;
+}
