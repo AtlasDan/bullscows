c7308a685dd2e6591542ed9f51b7e645f78df51e
AtlasDan <nikolaevdaniiloleg@gmail.com>
2021-01-03 23:22:12 +0700

    The number.h library was created with basic directives

---
+++ src/sources/number.h
@@ -0,0 +1,13 @@
+#ifndef BULLCOW_NUMBER_H
+#define BULLCOW_NUMBER_H
+
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <vector>
+
+#include "message.h"
+
+using namespace std;
+
+#endif // BULLCOW_NUMBER_H
