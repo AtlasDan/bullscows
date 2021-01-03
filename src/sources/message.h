2362ceb04562ef62b4f806c2326346da8ac4cc0b
AtlasDan <nikolaevdaniiloleg@gmail.com>
2021-01-04 00:02:17 +0700

    The message.h file declares procedures for displaying specific messages to notify the user

--- src/sources/message.h
+++ src/sources/message.h
@@ -5,6 +5,19 @@
 
 using namespace std;
 
+void mainMenu();
 void gameHeader();
+void gameOptionsMenu(int lengthNum, int maxAttempt);
+void gameRules();
+void gameCredits();
+void invalidInput();
+void goodbye();
+void congratulate(int hiddenNum);
+void replaceLengthNum();
+void resultAttempt(int bulls, int cows);
+void inputNumber(int currentAttempt, int maxAttempt);
+void invalidAnswerInput(int min, int max);
+void lostGame();
+void replaceMaxAttempt();
 
 #endif // BULLCOW_PRINTMENU_H
